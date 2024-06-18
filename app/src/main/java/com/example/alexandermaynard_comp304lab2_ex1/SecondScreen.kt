03045994e06ceba37670e13639616eac5812b7a3
almaynard-rgb <almaynard2404@gmail.com>
2024-06-18 15:47:49 -0500

    Finished: -initial MainScreen (activity main) -created/integrated initial version of the options menu -added custom image to the project -Started the second screen

--- app/src/main/java/com/example/alexandermaynard_comp304lab2_ex1/SecondScreen.kt
+++ app/src/main/java/com/example/alexandermaynard_comp304lab2_ex1/SecondScreen.kt
@@ -1,36 +1,20 @@
 package com.example.alexandermaynard_comp304lab2_ex1
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SecondScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val enterBtn = findViewById<Button>(R.id.main_enter_btn)
-        enterBtn.setOnClickListener {
-            val nextActivityIntent = Intent(this, SecondScreen::class.java)
-            startActivity(nextActivityIntent)
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.options_menu, menu)
-        menu?.clear() //make sure there are no options on the main page to be selected
-        return true
     }
 }
