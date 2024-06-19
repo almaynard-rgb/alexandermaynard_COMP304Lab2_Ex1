6aeb559057462ee0914afb0c50ca60e221984fd0
almaynard-rgb <almaynard2404@gmail.com>
2024-06-19 12:18:19 -0500

    Adjustments to strings.xml, activity_main.xml and finished content for activity_apartment.xml/Apartment.kt. Also created the recycler view to be used by all the home type screens.

--- app/src/main/java/com/example/alexandermaynard_comp304lab2_ex1/Apartment.kt
+++ app/src/main/java/com/example/alexandermaynard_comp304lab2_ex1/Apartment.kt
@@ -5,8 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.collections.ArrayList
 
 class Apartment : AppCompatActivity() {
+
+    private var apartmentList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,23 @@ class Apartment : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val apartmentRecyclerList = findViewById<RecyclerView>(R.id.house_type_recycler_view)
+
+        val apartmentRecyclerAdapter = HouseItemsRecyclerViewAdapter(this, apartmentList)
+        apartmentRecyclerList.adapter = apartmentRecyclerAdapter
+
+        //apartmentList.add()
+
+
+        //add dividers for the recycler list items
+        apartmentRecyclerList.addItemDecoration(DividerItemDecoration(applicationContext, DividerItemDecoration.VERTICAL))
+
+        val layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        apartmentRecyclerList.layoutManager = layoutManager
+
+        apartmentList.add("Apartment: 2 bedroom, 1 bath. 16 Marion Street, Winnipeg NB, Canada at $3000/month.")
+        apartmentList.add("Apartment: 1 bedroom, 1 bath. 24 Zander Street, Winnipeg MB, Canada at $1500/month.")
     }
 }
---
+++ app/src/main/java/com/example/alexandermaynard_comp304lab2_ex1/HouseItemsRecyclerViewAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.alexandermaynard_comp304lab2_ex1
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+
+internal class HouseItemsRecyclerViewAdapter(context: Context, private var houseTypeList: List<String>) :
+    RecyclerView.Adapter<HouseItemsRecyclerViewAdapter.MyViewHolder>() {
+
+    internal inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        //get the textview and checkbox for each row
+        var houseTypeList: TextView = view.findViewById(R.id.house_type_row_textview)
+        var houseTypeCheckBox: CheckBox = view.findViewById(R.id.house_type_row_checkbox)
+    }
+
+    //inflate view holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.house_item_recycler_view_row, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val houseType = houseTypeList[position]
+        holder.houseTypeList.text = houseType
+        //check if the checkboxes are checked or not
+    }
+    override fun getItemCount(): Int {
+        return houseTypeList.size
+    }
+}
