58d97c598811555b2fd4a9f0d9c1bebef498b927
almaynard-rgb <almaynard2404@gmail.com>
2024-06-18 19:16:08 -0500

    Finished initial functionality for the SecondScreen as well as linking the menu items to the other activities (empty at this point).

--- app/src/main/java/com/example/alexandermaynard_comp304lab2_ex1/SecondScreen.kt
+++ app/src/main/java/com/example/alexandermaynard_comp304lab2_ex1/SecondScreen.kt
@@ -1,6 +1,9 @@
 package com.example.alexandermaynard_comp304lab2_ex1
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,54 @@ class SecondScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        createMenuOptions(menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return menuItemSelected(item)
+    }
+
+    private fun createMenuOptions(menu: Menu) {
+        menu.add(0,0,0, "Apartment")
+        menu.add(0,1,1, "Detached Home")
+        menu.add(0,2,2, "Semi-Detached Home")
+        menu.add(0,3,3, "Condominium Apartment")
+        menu.add(0,4,4, "Townhouse")
+    }
+
+    private fun menuItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            0 -> {
+                val nextScreenIntent = Intent(this, Apartment::class.java)
+                startActivity(nextScreenIntent)
+                return true
+            }
+            1 -> {
+                val nextScreenIntent = Intent(this, DetachedHome::class.java)
+                startActivity(nextScreenIntent)
+                return true
+            }
+            2 -> {
+                val nextScreenIntent = Intent(this, SemiDetachedHome::class.java)
+                startActivity(nextScreenIntent)
+                return true
+            }
+            3 -> {
+                val nextScreenIntent = Intent(this, CondominiumApartment::class.java)
+                startActivity(nextScreenIntent)
+                return true
+            }
+            4 -> {
+                val nextScreenIntent = Intent(this, Townhouse::class.java)
+                startActivity(nextScreenIntent)
+                return true
+            }
+        }
+        return false
     }
 }
